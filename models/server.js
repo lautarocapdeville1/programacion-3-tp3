705a438a76cc6388c4dc44e83c9a0f7d288938bd
BautiCutini <bauticutinii@gmail.com>
2026-05-11 23:27:20 -0300

    Archivos iniciales

--- models/server.js
+++ models/server.js
@@ -1,1 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.middleware();
+        this.routes();
+    }
+
+    middleware() {
+        this.app.use(cors());
+    }
+
+    routes() {
+        this.app.use('/servicios', require('../routes/serviciosRoutes'))
+
+    // manejo de errores
+    this.app.use((req, res, next) => {
+        return res.status(400).json({ msg: 'Error.' })
+    })
+    this.app.use((err, req, res, next) => {
+        console.error(err.stack)
+        return res.status(404).json({ msg: 'Error. Pagina no encontrada' })
+    })
+    this.app.use((err, req, res, next) => {
+        console.error(err.stack)
+        return res.status(500).json({ msg: 'Internal Server Error' })
+    })
+  }
+
+  listen () {
+    this.app.listen(this.port, () => {
+        console.log(`La API esta escuchando el el puerto: ${this.port}`)
+    })
+  }
+}
+
+module.exports = Server
