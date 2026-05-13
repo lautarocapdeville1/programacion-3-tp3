1df80de3180106495f91ac9c5d0cfb8cd2e6d5d0
lautarocapdeville1 <lautarocapdeville0@gmail.com>
2026-05-13 12:36:14 -0300

    Revert "Lautaro capdeville"

--- routes/equipoRouters.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const { getEquipo } = require('../controllers/equipoControlles')
-
-const router = Router()
-
-router.get('/', getEquipo)
-
-module.exports = router
